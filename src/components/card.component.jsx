f5a12794b253cfa673e75a2cf6e618247ab2f5f6
Sid <tankasala.siddhartha@gmail.com>
2023-05-01 17:48:21 +0530

    initial commit

---
+++ src/components/card.component.jsx
@@ -0,0 +1,15 @@
+import {Component} from "react";
+import '../card.css'
+class Card extends Component {
+    render() {
+        const {name, email, id} = this.props.monster;
+        return (
+            <div className='card-container' key={id}>
+                <img alt={`monster, ${name}`} src={`https://robohash.org/${id}?set=set2`}/>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>);
+    }
+}
+
+export default Card
